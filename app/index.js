737d5aa7816dc7f3c505639797a68f0a7d1c7528
Cole Rath <grath90@gmail.com>
2018-07-31 15:41:16 -0500

    added Inventory to exports.

--- app/index.js
+++ app/index.js
@@ -3,3 +3,4 @@ exports.Orders = require('./orders/');
 exports.Reports = require('./reports/');
 exports.Feeds = require('./feeds/');
 exports.InboundShipments = require('./inbound-shipments/');
+exports.Inventory = require('./inventory/');
