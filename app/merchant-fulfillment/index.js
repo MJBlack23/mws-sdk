0225b0cc569a70931d7cef38d7d2d9466765dd66
Cole Rath <grath90@gmail.com>
2020-11-12 14:56:46 -0600

    added base for merchant fulfillment

--- app/merchant-fulfillment/index.js
+++ app/merchant-fulfillment/index.js
@@ -63,6 +63,143 @@ class MerchantFulfillment extends MWS {
     }
   }
 
+  /**
+   * @param {string} params.OrderId - Required - Amaonz defined order id.
+   * @param {string} params.ShippingServiceId - Required - The identifer for the carrier shipping serive that the seller chose.
+   * This was returned by a call to GetEligibleShippingServices
+   * @param {object} params.ShipFromAddress - Required - The address from which the shipment ships.
+   */
+  getAdditionalSellerInputs(params) {
+    const request = Object.assign({}, this.BASE_REQUEST);
+    request.query.Action = 'GetAdditionalSellerInputs';
+    request.query.MarketplaceId = this.marketplaceId;
+
+    try {
+      if (!params.OrderId || !params.ShippingServiceId || !params.ShipFromAddress) {
+        throw new Error('Order Id, Shipping Service Id, and Ship from Address are all required for this operation');
+      } else if (typeof params.OrderId !== 'string') {
+        throw new Error('Order id must be a string and a valid amazon order id');
+      } else if (typeof params.ShippingServiceId !== 'string') {
+        throw new Error('Shipping service id must be a string a valid shipping service id returned from GetEligibleShippingServices');
+      } else if (typeof params.ShipFromAddress !== 'object') {
+        throw new Error('Ship from Address must be an object and an MWS Address Type');
+      }
+
+      request.query.OrderId = params.OrderId;
+      request.query.ShippingServiceId = params.ShippingServiceId;
+      request.query.ShipFromAddress = MerchantFulfillment.assignParams(request, params, 'ShipFromAddress')
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /**
+   * @param {string} params.ShipmentId - Required - amazon defined shipment identifier
+   */
+  async getShipment(params) {
+    const request = Object.assign({}, this.BASE_REQUEST);
+    request.query.Action = 'GetShipment';
+    request.query.MarketplaceId = this.marketplaceId;
+
+    try {
+      if (!params.ShipmentId) {
+        throw new Error('Shipment Id is required for this operation');
+      }
+
+      request.query.ShipmentId = params.ShipmentId;
+
+      const { headers, body } = await this.makeCall(request, true);
+
+      const json = await MerchantFulfillment.XMLToJSON(body);
+
+      if (json.$) {
+        delete json.$;
+      }
+
+      return { headers, body: json };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+   /**
+   * @param {string} params.ShipmentId - Required - amazon defined shipment identifier
+   */
+  async cancelShipment(params) {
+    const request = Object.assign({}, this.BASE_REQUEST);
+    request.query.Action = 'Cancel Shipment';
+    request.query.MarketplaceId = this.marketplaceId;
+
+    try {
+      if (!params.ShipmentId) {
+        throw new Error('Shipment Id is required for this operation');
+      }
+
+      request.query.ShipmentId = params.ShipmentId;
+
+      const { headers, body } = await this.makeCall(request, true);
+
+      const json = await MerchantFulfillment.XMLToJSON(body);
+
+      if (json.$) {
+        delete json.$;
+      }
+
+      return { headers, body: json };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /**
+   * @param {object} params.ShipmentRequestDetails - Required - Shipment information required for creating a shipment
+   * @param {string} params.ShippingServiceId - Required - An Amazon-defined shipping service identifier.
+   * This is the returned id of the selected shipping service resulting from the GetEligibleShippingServices call
+   * @param {object} params.HazmatType - Optional - Hazardous materials options for a package
+   * @param {string} params.LabelFormatOption - Optional - Label options
+   * @param {object} params.ShipmentLevelSellerInputsList - Optional - A list of additional seller inputs, at the shipment level.
+   */
+  async createShipment(params) {
+    const request = Object.assign({}, this.BASE_REQUEST);
+    request.query.Action = 'CreateShipment';
+    request.query.MarketplaceId = this.marketplaceId;
+
+    try {
+      if (!params.ShipmentRequestDetails || !params.ShippingServiceId) {
+        throw new Error('Shipment Request Details and Shipping Service Id are required for this operation');
+      } else if (typeof params.ShipmentRequestDetails !== 'object') {
+        throw new Error('Shipment Request details is an object. Please refer to MWS documentation');
+      } else if (typeof params.ShippingServiceId !== 'string') {
+        throw new Error('Shipment Service Id must be a string type and must be a valid id returned from Amazon');
+      }
+
+      const { ShipmentRequestDetails } = params;
+      _.keys(ShipmentRequestDetails).forEach((key) => {
+        if (typeof ShipmentRequestDetails[key] === 'string') {
+          request.query[`ShipmentRequestDetails.${key}`] = ShipmentRequestDetails[key];
+        } else if (key === 'ItemList') {
+          const items = ShipmentRequestDetails[key];
+          MerchantFulfillment.assignItems(request, items, 'ShipmentRequestDetails');
+        } else {
+          const object = ShipmentRequestDetails[key];
+          MerchantFulfillment.assignParams(request, object, key, 'ShipmentRequestDetails');
+        }
+      });
+
+      const { headers, body } = await this.makeCall(request, true);
+
+      const json = await MerchantFulfillment.XMLToJSON(body);
+
+      if (json.$) {
+        delete json.$;
+      }
+
+      return { headers, body: json };
+    } catch (e) {
+      throw e;
+    }
+  }
+
   static assignParams(request, object, child, parent) {
     _.keys(object).forEach((key) => {
       request.query[`${parent}.${child}.${key}`] = object[key];
